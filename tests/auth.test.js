2357af05ad9177ea77a37ec2e59f1f1ecc60abde
KKA-0 <karanyobro@gmail.com>
2024-01-26 23:19:33 +0530

    fix final

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -1,40 +1,38 @@
 const request = require('supertest');
-const mongoose = require('mongoose')
-const userModel = require('../models/auth.schema')
-const server = require('../serverTest')
+const mongoose = require('mongoose');
+const userModel = require('../models/auth.schema');
+const server = require('../serverTest');
 const req = {
-    body: {
-        username: "test",
-        email: "_test_@jest.com",
-        password: "^%$&^%#$*^%$^%$*"
-    }
-}
+  body: {
+    username: 'test',
+    email: '_test_@jest.com',
+    password: '^%$&^%#$*^%$^%$*',
+  },
+};
 
-
-
-describe("Auth Tests", () => {
-
-    beforeEach((done) => {
-  mongoose.connect("mongodb://localhost:27017/JestDB",
-    { useNewUrlParser: true, useUnifiedTopology: true }, () =>done())
-    
-});
-
-    afterEach((done) => {    
-        mongoose.connection.db.dropDatabase(() => {
-            mongoose.connection.close(() => done());
-        });
+describe('Auth Tests', () => {
+  beforeEach(async () => {
+    await mongoose.connect("mongodb://localhost:27017/JestDB", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+  });
+  
+  afterEach(async () => {
+    // Delete the database
+    await mongoose.connection.db.dropDatabase();
+    
+    // Disconnect from the database
+    await mongoose.disconnect();
+  });
+      
 
-    it("POST /api/user", async () => {
-        await request(server).post("/api/user")
-        .send(req.body)
-        .expect(201)
-    })
-    it("POST /api/user", async () => {
-        await userModel.create(req.body);
-        await request(server).post("/api/user")
-        .send(req.body)
-        .expect(200)
-    })
-})
+  it('POST /api/user', async () => {
+    await request(server).post('/api/user').send(req.body).expect(201);
+  });
+
+  it('POST /api/user', async () => {
+    await userModel.create(req.body);
+    await request(server).post('/api/user').send(req.body).expect(200);
+  });
+});
